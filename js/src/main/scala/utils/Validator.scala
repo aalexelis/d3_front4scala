7bebe7458217ed16a08da30efb67c33930f6a47b
andreas@bizreach.co.jp <andreas@bizreach.co.jp>
2017-11-10 13:12:04 +0900

    refactored with shared validator

--- js/src/main/scala/utils/Validator.scala
+++ js/src/main/scala/utils/Validator.scala
@@ -5,65 +5,21 @@ import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
 import scala.util.Try
 
 @JSExportTopLevel("Utils.Validator")
-object Validotor {
+object Validotor extends shared.Validator {
 
   @JSExport
-  def isValideEmail(s: String): Boolean = {
-    val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
-    s match {
-      case s if !minLength(s, 1) => false
-      case s if emailRegex.findFirstMatchIn(s).isDefined => true
-      case _ => false
-    }
-  }
+  override def isValideEmail(s: String): Boolean = super.isValideEmail(s)
 
   @JSExport
-  def isValidPhone(s: String): Boolean = {
-
-    /*
-     See https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
-     */
-    val phoneRegex =
-      """^(\+(\d{1,3}))(\d{10})(?: *x(\d+))?\s*$""".r
-
-    def sanitizedString(s: String) = s.replace("(0)", "").replace(" ", "").replace("(", "").replace(")", "").replace("-", "").replace("/", "")
-
-    s match {
-      case s if !minLength(s, 1) => false
-      case s if phoneRegex.findFirstMatchIn(sanitizedString(s)).isDefined => true
-      case _ => false
-    }
-  }
+  override def isValidPhone(s: String): Boolean = isValidPhone(s)
 
   @JSExport
-  def isValidDate(s: String): Boolean = {
-    def extractDate(s: String): Boolean = {
-      Try {
-        val d: Date = new Date(Date.parse(s))
-        val t = d.toISOString()
-        t
-      }.isSuccess
-    }
-    s match {
-      case s if !minLength(s, 1) => false
-      case s if extractDate(s) => true
-      case _ => false
-    }
-  }
+  override def isValidDate(s: String): Boolean = isValidDate(s)
 
   @JSExport
-  def maxLength(s: String, length: Int): Boolean = s match {
-    case null => false
-    case s if s.trim.isEmpty => false
-    case s if s.size <= length => true
-    case _ => false
-  }
+  override def maxLength(s: String, length: Int): Boolean = maxLength(s, length)
 
   @JSExport
-  def minLength(s: String, length: Int): Boolean = s match {
-    case null => false
-    case s if s.trim.isEmpty => false
-    case s if s.size >= length => true
-    case _ => false
-  }
+  override def minLength(s: String, length: Int): Boolean = super.minLength(s, length)
+  
 }
